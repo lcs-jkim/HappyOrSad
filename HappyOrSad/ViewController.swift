4b4c7bfb1fc178c19f588dcce1a10174a2cf434a
Amanda <jkim@lcs.on.ca>
2019-11-19 12:43:42 -0500

    Got the unicode values printed and now started switch statement. Need to write down which unicode values are happy or sad emojis

--- HappyOrSad/ViewController.swift
+++ HappyOrSad/ViewController.swift
@@ -18,13 +18,25 @@ class ViewController: UIViewController {
 // MARK PROPERTIES
     @IBOutlet weak var TextField: UITextField!
     @IBOutlet weak var AnswerBox: UILabel!
-    
+    @IBOutlet weak var ErrorMessage: UILabel!
     
     @IBAction func Analyze(_ sender: Any) {
         
-        guard TextField.text != nil else {
+        guard let StringTextField = TextField.text else {
+            ErrorMessage.text = ("Please enter your text.")
             return
-            }
+        }
+        
+        for scalar in StringTextField.unicodeScalars {
+            print(scalar.value)
+        }
+        
+        switch StringTextField {
+        case <#pattern#>:
+            <#code#>
+        default:
+            AnswerBox.text = ("I'm not sure")
+        }
     
     }
     
